e04657bb77a956f87a2c16f249b3446aaaefbe05
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-05-10 02:44:28 +0300

    refactor: move user questions to variable

--- script.php
+++ script.php
@@ -1,4 +1,9 @@
 <?php
+require_once 'Models/FileSender.php';
+require_once 'Services/BoredApiService.php';
+require_once 'Controllers/MainController.php';
+require_once 'Views/ConsoleInputView.php';
+
 define('FILE_SENDER', 'file');
 define('CONSOLE_SENDER', 'console');
 define(
@@ -6,32 +11,36 @@ define(
 	["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
 );
 
-require_once 'Models/FileSender.php';
-require_once 'Services/BoredApiService.php';
-require_once 'Controllers/MainController.php';
-require_once 'Views/ConsoleInputView.php';
+$questions = [
+	"getParticipantsNumber" => "Enter a number of participants (0-8): ",
+	"getActivityType" => "Enter an activity. Available activities:\n" . implode(", ", ACTIVITIES) . ": ",
+	"getSenderType" => "Choose where to output the received data (file, console): "
+];
 
-$participantsNumber = ConsoleInputView::getInput("Enter a number of participants (0-8): ");
+$participantsNumber = ConsoleInputView::getInput($questions['getParticipantsNumber']);
+
 while (!is_numeric($participantsNumber) || $participantsNumber < 0 || $participantsNumber > 8) {
 	echo "Invalid number of participants: $participantsNumber. It should be a number between 0 and 8." . PHP_EOL;
 	echo "Please try again!" . PHP_EOL;
-	$participantsNumber = ConsoleInputView::getInput("Enter a number of participants (0-8): ");
+	$participantsNumber = ConsoleInputView::getInput($questions['getParticipantsNumber']);
 }
 echo "Number of participants is $participantsNumber" . PHP_EOL;
 
-$activityType = strtolower(ConsoleInputView::getInput("Enter an activity. Available activities:\n" . implode(", ", ACTIVITIES) . ": "));
+$activityType = strtolower(ConsoleInputView::getInput($questions['getActivityType']));
+
 while (!in_array($activityType, ACTIVITIES)) {
 	echo "Invalid activity: $activityType." . PHP_EOL;
 	echo "Please try again!" . PHP_EOL;
-	$activityType = strtolower(ConsoleInputView::getInput("Enter an activity. Available activities:\n" . implode(", ", ACTIVITIES) . ": "));
+	$activityType = strtolower(ConsoleInputView::getInput($questions['getActivityType']));
 }
 echo "You've chosen the next activity - $activityType" . PHP_EOL;
 
-$senderType = strtolower(ConsoleInputView::getInput("Choose where to output the received data: file, console: "));
+$senderType = strtolower(ConsoleInputView::getInput($questions['getSenderType']));
+
 while ($senderType !== FILE_SENDER && $senderType !== CONSOLE_SENDER) {
 	echo "Invalid output target: $senderType" . PHP_EOL;
 	echo "Please try again!" . PHP_EOL;
-	$senderType = strtolower(ConsoleInputView::getInput("Choose where to output the received data: file, console: "));
+	$senderType = strtolower(ConsoleInputView::getInput($questions['getSenderType']));
 }
 echo "You've chosen the next output - $senderType" . PHP_EOL;
 
